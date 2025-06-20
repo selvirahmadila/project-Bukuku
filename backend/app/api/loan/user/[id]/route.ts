49c9c344cd9c104a24f791496ebaa5669f312cf8
selvirahmadila <selvirahmadila523@gmail.com>
2025-06-20 11:41:41 +0700

    selvi backend : membuat api loan/user untuk fontend : membuat data peminjam

---
+++ backend/app/api/loan/user/[id]/route.ts
@@ -0,0 +1,14 @@
+// /api/loan/user/[id]/route.ts (GET - riwayat user)
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+interface Params { params: { id: string } }
+
+export async function GET(_: NextRequest, { params }: Params) {
+  const id = parseInt(params.id);
+  const history = await prisma.loan.findMany({
+    where: { userId: id },
+    include: { book: true }
+  });
+  return NextResponse.json(history);
+}
