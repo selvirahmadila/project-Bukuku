a70bbea915f12d1ec32f01cbf51c2f2ffc062c94
selvirahmadila <selvirahmadila523@gmail.com>
2025-06-20 11:35:37 +0700

    selvi backend : membuat api 'admin/login' untuk cms

---
+++ backend/app/api/admin/login/route.ts
@@ -0,0 +1,12 @@
+// /api/admin/login/route.ts
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const { username, password } = await req.json();
+
+  if (username === "admin" && password === "admin123") {
+    return NextResponse.json({ success: true });
+  } else {
+    return NextResponse.json({ success: false, message: "Login gagal" }, { status: 401 });
+  }
+}
