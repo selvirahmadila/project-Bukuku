6089e6cccef175b7476a2148bf3177b70605999a
selvirahmadila <selvirahmadila523@gmail.com>
2025-06-20 17:02:22 +0700

    selvi backend : membuat api book/[id] untuk admin

--- backend/app/api/admin/book/route.ts
+++ backend/app/api/admin/book/route.ts
@@ -15,16 +15,9 @@ export async function POST(req: NextRequest) {
   const penulis = formData.get("penulis") as string;
   const kategori = formData.get("kategori") as string;
   const status = formData.get("status") as string;
-  const file = formData.get("coverimage") as File;
-
-  const bytes = await file.arrayBuffer();
-  const buffer = Buffer.from(bytes);
-  const filename = `${Date.now()}_${file.name}`;
-  const filePath = path.join(process.cwd(), "public", "uploads", filename);
-  await writeFile(filePath, buffer);
-
+  
   const newBook = await prisma.book.create({
-    data: { judul, penulis, kategori, status, coverimage: `/uploads/${filename}` }
+    data: { judul, penulis, kategori, status}
   });
 
   return NextResponse.json(newBook);
