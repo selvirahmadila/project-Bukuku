5e87bba2bd70280c089e1b4046f310b67c8ac358
selvirahmadila <selvirahmadila523@gmail.com>
2025-06-20 17:08:44 +0700

    memperbarui fungsi get di api admin/loan

--- backend/app/api/admin/loan/route.ts
+++ backend/app/api/admin/loan/route.ts
@@ -2,3 +2,9 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
+export async function GET() {
+  const loans = await prisma.loan.findMany({
+    include: { user: true, book: true }
+  });
+  return NextResponse.json(loans);
+}
